b2ed515a1b87f4a949095431d39b9a8baa4b27d4
jayant4 <62827014+jayant4@users.noreply.github.com>
2020-08-17 22:31:01 +0530

    Update Binomial_Distribution.py

--- Binomial_Distribution.py
+++ Binomial_Distribution.py
@@ -5,12 +5,15 @@ import streamlit as st
 st.title('Calculate Binomial Distribution')
 
 
-n = float((st.text_input(" Enter Number of trials(n) : ")) ) # 10
-p = float((st.text_input("   Enter Probability of success on a single trial p (0.0 to 1.0) : ")) ) # 0.5
-x = float(st.text_input("   Enter Number of successes (x)	 : "))  # 3
+n = ((st.text_input(" Enter Number of trials(n) : ")) ) # 10
+p = ((st.text_input("   Enter Probability of success on a single trial p (0.0 to 1.0) : ")) ) # 0.5
+x = (st.text_input("   Enter Number of successes (x)	 : "))  # 3
 
 
 if (st.button("calculate")):
+ n=float(n)
+ p=float(p)
+ x=float(x)
  f"""
      Binomial probability   P(X = x) : {scipy.stats.binom.pmf(x,n,p)}\n
      Cumulative probability P(X < x) : {scipy.stats.binom.cdf(x,n,p)-scipy.stats.binom.pmf(x,n,p)}\n
